11de10c11ea94471099bcef4b249b0539139c8f3
Antarcticpx <antarcticpx@gmail.com>
2024-03-14 17:29:53 +0700

    Configured  Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 
 const poppins = Poppins({ 
   weight: ['400', '700'],
@@ -23,6 +24,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+       
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-HNMHYG92T0"></Script>
+        <Script id="google-analytics">
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-HNMHYG92T0');
+            `
+          }
+        </Script>
+
+      </head>
       <link rel="icon" href="/img/favicon.ico" sizes="any" />
       <body className={`bg-zinc-900 ${poppins.className}`}>{children}</body>
     </html>
